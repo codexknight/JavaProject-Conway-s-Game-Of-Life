53e87b78f6e9aaa9933738bafc828b0545cbdd5a
codexknight <dfotache@gmail.com>
2019-12-27 13:12:15 +0200

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
