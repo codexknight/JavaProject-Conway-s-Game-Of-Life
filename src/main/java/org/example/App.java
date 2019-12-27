54c978236adce3b19e72f78ddcd379b3abcfac04
codexknight <dfotache@gmail.com>
2019-12-27 13:49:00 +0200

    Create the buttons and checkbox

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,11 @@ package org.example;
 
 import javafx.application.Application;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -9,9 +14,36 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    private Button stopGoButton;
+    private Button nextButton;
+    private Button randomButton;
+    private Button clearButton;
+    private Button quitButton;
+
+    private CheckBox fastCheckbox;
+
+
     @Override
     public void start(Stage stage) {
 
+        stopGoButton = new Button("Start");
+        nextButton = new Button("One Step");
+        randomButton = new Button("Random Fill");
+        clearButton = new Button("Clear");
+        quitButton = new Button("Quit");
+
+        fastCheckbox = new CheckBox("Fast");
+
+        HBox bottom = new HBox(20, stopGoButton, fastCheckbox, nextButton, randomButton, clearButton, quitButton);
+
+        BorderPane root = new BorderPane();
+        root.setBottom(bottom);
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Game of Life");
+        stage.setResizable(false);
         stage.show();
     }
 
