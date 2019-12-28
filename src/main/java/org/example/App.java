cede95ed38ecc77bb007dbec552d016e3c0ece78
codexknight <dfotache@gmail.com>
2019-12-28 16:02:50 +0200

    added clearButton and quitButton

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,15 @@
 package org.example;
-
 import javafx.application.Application;
-
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
+//////////////////////////////////////////////////////
 public class App extends Application {
 
     private Button stopGoButton;
@@ -23,24 +20,43 @@ public class App extends Application {
 
     private CheckBox fastCheckbox;
 
+    private static final int GRID_SIZE = 100;
 
+    private MosaicCanvas lifeBoard;
+
+    private boolean[][] alive;
+
+
+//////////////////////////////////////////////////////
     @Override
     public void start(Stage stage) {
-
+//****************************************************
+        int cellSize = 800 / GRID_SIZE; //8px
+        lifeBoard = new MosaicCanvas(GRID_SIZE, GRID_SIZE, cellSize, cellSize);
+        lifeBoard.setStyle("-fx-border-color:darkgray; -fx-border-width:3px");
+//****************************************************
         stopGoButton = new Button("Start");
         nextButton = new Button("One Step");
         randomButton = new Button("Random Fill");
+
         clearButton = new Button("Clear");
+        clearButton.setOnAction(e->{
+            alive = new boolean[GRID_SIZE][GRID_SIZE];
+            showBoard();
+        });
+
         quitButton = new Button("Quit");
+        quitButton.setOnAction(e -> System.exit(0));
 
         fastCheckbox = new CheckBox("Fast");
-
+//*****************************************************
         HBox bottom = new HBox(20, stopGoButton, fastCheckbox, nextButton, randomButton, clearButton, quitButton);
         bottom.setStyle("-fx-padding:8px; -fx-border-color:darkgray; -fx-border-width:3px 0 0 0");
         bottom.setAlignment(Pos.CENTER);
 
         BorderPane root = new BorderPane();
         root.setBottom(bottom);
+        root.setCenter(lifeBoard);
 
         Scene scene = new Scene(root);
 
@@ -48,6 +64,20 @@ public class App extends Application {
         stage.setTitle("Game of Life");
         stage.setResizable(false);
         stage.show();
+    }
+
+    private void showBoard() {
+        lifeBoard.setAutopaint(false);
+        for (int r = 0; r < GRID_SIZE; r++) {
+            for (int c = 0; c < GRID_SIZE; c++) {
+                if (alive[r][c]) {
+                    lifeBoard.setColor(r, c, Color.WHITE);
+                } else {
+                    lifeBoard.setColor(r, c, null);
+                }
+            }
+        }
+        lifeBoard.setAutopaint(true);
     }
 
     public static void main(String[] args) {
